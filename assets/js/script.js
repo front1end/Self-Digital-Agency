51b9d10d6f78ffa5302661fadcaf7e2ccaaf81ed
Mr Omar <mromar.work@gmail.com>
2022-08-23 15:10:27 +0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,121 @@
+'use strict';
+
+/**
+ * navbar toggle
+ */
+
+const header = document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const navLinks = document.querySelectorAll("[data-navbar-link]");
+
+navToggleBtn.addEventListener("click", function () {
+  this.classList.toggle("active");
+  header.classList.toggle("nav-active");
+});
+
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener("click", function () {
+    header.classList.toggle("nav-active");
+    navToggleBtn.classList.toggle("active");
+  });
+}
+
+
+
+/**
+ * header scroll active state & go to top
+ */
+
+const goTopBtn = document.querySelector("[data-go-top]");
+
+window.addEventListener("scroll", function () {
+
+  if (window.scrollY >= 100) {
+    header.classList.add("active");
+    goTopBtn.classList.add("active");
+  } else {
+    header.classList.remove("active");
+    goTopBtn.classList.remove("active");
+  }
+
+});
+
+/*Portfolio*/
+
+// gallery item filter
+  
+const filterButtons = document.querySelector("#filter-btns").children;
+const items = document.querySelector(".portfolio-gallery").children;
+  
+for (let i = 0; i < filterButtons.length; i++) {
+    filterButtons[i].addEventListener("click", function () {
+        for (let j = 0; j < filterButtons.length; j++) {
+            filterButtons[j].classList.remove("active")
+        }
+        this.classList.add("active");
+        const target = this.getAttribute("data-target")
+  
+        for (let k = 0; k < items.length; k++) {
+            items[k].style.display = "none";
+            if (target == items[k].getAttribute("data-id")) {
+                items[k].style.display = "block";
+            }
+            if (target == "all") {
+                items[k].style.display = "block";
+            }
+        }
+  
+    })
+}
+
+
+// Handle Image Gallery
+const galleryMask = document.querySelector(".gallery-mask");
+const maskImage = document.querySelector(".mask-image");
+const galleryImgs = document.querySelectorAll(".image-item");
+
+galleryMask.addEventListener("click", () => {
+	galleryMask.classList.add("mask-off");
+});
+
+galleryImgs.forEach((item) => {
+	item.addEventListener("click", () => {
+		let styleValue = item.getAttribute("style");
+		maskImage.setAttribute(
+			"src",
+			styleValue.slice(styleValue.indexOf(": url(") + 7, -3)
+		);
+		galleryMask.classList.remove("mask-off");
+	});
+});
+
+
+
+
+
+/*Bizden gorus al */
+  // Get the modal
+  var modal = document.getElementById("myModal");
+  
+  // Get the button that opens the modal
+  var btn = document.getElementById("myBtn");
+  
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("qapat")[0];
+  
+  // When the user clicks the button, open the modal 
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+  
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
